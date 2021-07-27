8055666184735ccea140c85df519bdb6fd66f9a4
MarceloRavache <jm.ravache@outlook.com>
2021-07-27 19:34:31 -0300

    update: added routes for users

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+import Users from "./routes/User.routes";
+
 // configs
 
 const app = express();
@@ -10,5 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+// routes
+
+app.use(Users);
 
 export default app;
