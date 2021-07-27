ddd912cfa37ab5e5e5042b37533c62ed4c020bf6
MarceloRavache <jm.ravache@outlook.com>
2021-07-27 20:05:03 -0300

    add: added routes of create and update for users

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/User.controller";
+import {
+  createUser,
+  getUsers,
+  updateUser,
+} from "../controllers/User.controller";
 
 const router = Router();
 
 router.get("/users", getUsers);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
 
 export default router;
